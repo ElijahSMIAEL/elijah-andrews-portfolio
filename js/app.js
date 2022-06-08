d60cff0d473aed3fbe0d0aeb07025ace2b316467
Endrews <elijah.smiael@gmail.com>
2022-06-08 13:11:42 -0500

    creates design for dark mode toggle

---
+++ js/app.js
@@ -0,0 +1,16 @@
+homeBody = document.querySelector('#home-body')
+homeHeader = document.querySelector('.home-header')
+toggleModeButton = document.querySelector('.dark-light-button')
+
+
+let isDark = false
+
+toggleModeButton.addEventListener('click', darkMode)
+
+function darkMode(evt) {
+  
+}
+
+function lightMode() {
+
+}
