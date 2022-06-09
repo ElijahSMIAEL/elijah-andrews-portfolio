f6c1135ee80521ddc6fd4bb854f0b233cb05448c
Endrews <elijah.smiael@gmail.com>
2022-06-09 09:03:40 -0500

    Begins dark mode functionality

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-homeBody = document.querySelector('#home-body')
+homeBody = document.querySelector('.home-body')
 homeHeader = document.querySelector('.home-header')
 toggleModeButton = document.querySelector('.dark-light-button')
+sideBar = document.querySelector('.preview-sidebar')
+previewIcons = document.querySelectorAll(".icon")
 
-
-let isDark = false
 
 toggleModeButton.addEventListener('click', darkMode)
 
 function darkMode(evt) {
-  
-}
-
-function lightMode() {
-
+  homeBody.classList.toggle("dark-mode-body")
+  homeHeader.classList.toggle("dark-mode")
+  toggleModeButton.classList.toggle("dark-mode")
+  sideBar.classList.toggle("dark-mode")
+  previewIcons.classList.toggle("dark-mode")
 }
