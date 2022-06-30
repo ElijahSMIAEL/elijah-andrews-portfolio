ceee2533cedf298809f9ec0c691d11a08e27f7a3
Endrews <elijah.smiael@gmail.com>
2022-06-30 18:36:15 -0500

    add new work

--- js/works.js
+++ js/works.js
@@ -5,6 +5,13 @@ const works = [
     gitHub: 'https://github.com/ElijahSMIAEL/ttt-weekend',
     image: '../Images/TicTacToe.png',
     description: 'A simple, interactive browser game',
+  },
+  {
+    name: 'Gamers Assemble',
+    deployed: 'https://gamers-assembly.herokuapp.com/',
+    gitHub: 'https://github.com/ElijahSMIAEL/gamers-assemble',
+    image: '../Images/GamersAssemble.png',
+    description: 'A group finding, MEN-Stack application for gamers'
   }
 ]
 
